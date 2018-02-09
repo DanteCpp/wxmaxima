e127601cec25a0484bdf449e1c46b37214622a7f
Gunter Königsmann <gunter@peterpall.de>
2018-02-09 23:03:00 +0100

    Now all icons are included in the binary. But I am not sure if the network icon will always be in scale.

--- art/statusbar/generate_pngs.sh
+++ art/statusbar/generate_pngs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 for i in *.svg; do
-  for j in 128; do
-    inkscape -e `basename $i .svg`.png -w $j $i
-    optipng -o 9 `basename $i .svg`.png
+  for j in 128 192; do
+    inkscape -e `basename $i .svg`.$j.png -w $j $i
+    optipng -o 9 `basename $i .svg`.$j.png
   done
 done
 
